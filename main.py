bb667aa6afe0483321c3fbde410b51c85ce20e26
sxf51 <sxf5166@163.com>
2025-05-03 20:29:16 +1000

    data读取

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ import time
 import mujoco
 import mujoco.viewer as viewer
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 
 from casadi import SX, vertcat
 
 m = mujoco.MjModel.from_xml_path('./crazyfile/scene.xml')
 d = mujoco.MjData(m)
+
+
+gravity = 9.8066        # 重力加速度 单位m/s^2
+mass = 0.033            # 飞行器质量 单位kg
+Ct = 3.25e-4            # 电机推力系数 (N/krpm^2)
+Cd = 7.9379e-6          # 电机反扭系数 (Nm/krpm^2)
+
+arm_length = 0.065/2.0  # 电机力臂长度 单位m
+max_thrust = 0.1573     # 单个电机最大推力 单位N (电机最大转速22krpm)
+max_torque = 3.842e-03  # 单个电机最大扭矩 单位Nm (电机最大转速22krpm)
+
+# 仿真周期 100Hz 10ms 0.01s
+dt = 0.01
 
 
 paused = False
@@ -17,12 +32,56 @@ def key_callback(keycode):
         global paused
         paused = not paused
 
-
 with mujoco.viewer.launch_passive(m, d, key_callback=key_callback) as viewer:
     while viewer.is_running():
         if not paused:
+            if (d.qpos[2] < 1):
+                d.actuator('motor1').ctrl[0] = 0.52
+                d.actuator('motor2').ctrl[0] = 0.52
+                d.actuator('motor3').ctrl[0] = 0.52
+                d.actuator('motor4').ctrl[0] = 0.52
+            else:
+                d.actuator('motor1').ctrl[0] = 0.51
+                d.actuator('motor2').ctrl[0] = 0.51
+                d.actuator('motor3').ctrl[0] = 0.51
+                d.actuator('motor4').ctrl[0] = 0.51
             mujoco.mj_step(m, d)
             viewer.sync()
+
+
+log_count = 0
+def control_callback(m, d):
+    global log_count, gravity, mass
+    _pos = d.qpos
+    _vel = d.qvel
+    _sensor_data = d.sensordata
+    gyro_x = _sensor_data[0]
+    gyro_y = _sensor_data[1]
+    gyro_z = _sensor_data[2]
+    acc_x = _sensor_data[3]
+    acc_y = _sensor_data[4]
+    acc_z = _sensor_data[5]
+    quat_w = _sensor_data[6]
+    quat_x = _sensor_data[7]
+    quat_y = _sensor_data[8]
+    quat_z = _sensor_data[9]
+    quat = np.array([quat_x, quat_y, quat_z, quat_w])  # x y z w
+    omega = np.array([gyro_x, gyro_y, gyro_z])         # 角速度
+    # 构建当前状态
+    current_state = np.array([_pos[0], _pos[1], _pos[2], quat[3], quat[0], quat[1], quat[2], _vel[0], _vel[1], _vel[2], omega[0], omega[1], omega[2]])
+    # 位置控制模式 目标位点
+    goal_position = np.array([0.0, 0.0, 0.5])
+
+    # NMPC Update
+    d.actuator('motor1').ctrl[0] = 0.5
+    d.actuator('motor2').ctrl[0] = 0.5
+    d.actuator('motor3').ctrl[0] = 0.5
+    d.actuator('motor4').ctrl[0] = 0.5
+
+    log_count += 1
+    if log_count >= 50:
+        log_count = 0
+        # 这里输出log
 
 def export_model():
     model_name = 'crazyflie'
